69c84eb524cfa4fe044c165d6893c57ffb00382a
sprin <sprin>
2013-07-03 01:56:49 -0700

    Adapt demo to latest Qoogr core, qgr-cntrl-checkbox, and qgr-graph-barchart

--- src/main.js
+++ src/main.js
@@ -40,40 +40,100 @@ requirejs([
     'underscore',
     'backbone',
     'qoogr/src/qoogr',
-    'qgr-cntrl-checkbox/src/qgr-cntrl-checkbox'
+    'qgr-cntrl-checkbox/src/qgr-cntrl-checkbox',
+    'qjb-qexec-array/src/qjb-qexec-array'
   ],
-  function(require, $, _, Backbone, Qoogr, QgrCheckbox) {
+  function(require, $, _, Backbone, Qoogr, QgrCheckbox, QueryExecutor) {
 
 
   var ControlsView = Qoogr.ControlsView.extend({
 
     initialize: function(options) {
-      this.global_q = this.options.global_q;
-      this.render();
+      var t = this;
+      t.render();
       // Set up individual filter widgets here.
-      this.color_choices = new QgrCheckbox.CheckboxChoices([
-        {choice_val: 'qoral'},
-        {choice_val: 'qrimson'},
-        {choice_val: 'xhrtreuse'}
-      ]);
-      this.checkboxes_view  = new QgrCheckbox.CheckboxChoicesView({
+      t.color_choices = new QgrCheckbox.CheckboxChoices(
+        [
+          {choice_val: 'qoral'},
+          {choice_val: 'qrimson'},
+          {choice_val: 'xhrtreuse'}
+        ],
+        {
+          col: 'color'
+        }
+      );
+
+      // Proxy change events on contained data models to listeners.
+      t.proxy(t.color_choices, 'change');
+
+      t.color_choice_view  = new QgrCheckbox.CheckboxChoicesView({
         el: '#color-choices',
         choices: this.color_choices,
-        col: 'color',
-        global_q: this.global_q
       })
       .render();
     },
 
   });
 
+
+  var QueryMapper = Qoogr.QueryMapper.extend({
+
+    map_controls: function() {
+      var t = this;
+      // Build a new qtree by reading each control's data model.
+      // The following is a stub for a basic filter qtree.
+      t.qtree = {
+        select: {
+          where: {
+            and: []
+          }
+        }
+      }
+      // Alias the toplevel and subclause for brevity.
+      var and = t.qtree.select.where.and;
+
+      // Push subclauses from control data models into query tree.
+      and.push(t.controls.color_choices.get_subtree());
+
+      // Fire change event to alert listeners qtree has changed.
+      t.trigger('change');
+    }
+
+  });
+
+
+  var birds = [
+    {species: 'Waterfall Swift', color: 'qrimson', val: 1},
+    {species: 'Waterfall Swift', color: 'xhrtreuse', val: 2},
+    {species: 'Cave Swiftlet', color: 'qoral', val: 4},
+    {species: 'Cave Swiftlet', color: 'xhrtreuse', val: 2}
+  ];
+
+  var array_map = {
+    birds: birds,
+  }
+
+  var bird_graph_config = {
+    graph: 'qgr-graph-barchart/src/qgr-graph-barchart',
+    from: 'birds',
+    label: 'Birds'
+  }
+
+  var graph_config_map = {
+    birds: bird_graph_config
+  }
+
   var AppController = Backbone.View.extend({
 
     el: $('body'),
 
     initialize: function() {
-      this.qoogr = new Qoogr.Controller({
-        controls_class: ControlsView
+      this.qoogr = new Qoogr.QoogrView({
+        controls_class: ControlsView,
+        qmapper_class: QueryMapper,
+        qexec: QueryExecutor,
+        array_map: array_map,
+        graph_config_map: graph_config_map
       });
     },
 
